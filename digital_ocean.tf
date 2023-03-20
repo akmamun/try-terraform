4bd90fa391187b752a1ac00b7f6d535d0298997e
Al Mamun Khan <10064901+akmamun@users.noreply.github.com>
2023-03-20 17:41:23 +0600

    Update digital_ocean.tf

--- digital_ocean.tf
+++ digital_ocean.tf
@@ -8,12 +8,13 @@ terraform {
     }
   }
 }
-# Configure the DigitalOcean provider API Access Token
+
+# Create API token from DigitalOcean and Configure API Token
 provider "digitalocean" {
   token = "digitalocean_token"
 }
 
-# Network firewall define
+# Network firewall define, allow necessary port
 resource "digitalocean_firewall" "visie-server-firewall" {
   name = "visie-server-firewall"
 
@@ -48,12 +49,13 @@ resource "digitalocean_firewall" "visie-server-firewall" {
 
 }
 
+# Add ssh key for future ssh access 
 resource "digitalocean_ssh_key" "my_ssh_key" {
   name = "new_ssh_key"
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
-# Create a new Droplet
+# Create a new Droplet Configuration
 resource "digitalocean_droplet" "visie-server" {
   name       = "visie-server"
   region     = "sgp1"
@@ -65,18 +67,22 @@ resource "digitalocean_droplet" "visie-server" {
 
 }
 
+# create tags 
 resource "digitalocean_tag" "visie-tag" {
   name = "visie-tag"
 }
 
+# assined ip 
 output "droplet_ip_address" {
   value = digitalocean_droplet.visie-server.ipv4_address
-
 }
+
+# create reserved ip 
 resource "digitalocean_reserved_ip" "visie-reserved-ip" {
   region = "sgp1"
 } 
 
+# assinded reserved ip to droplet
 resource "digitalocean_reserved_ip_assignment" "assigned-ip" {
   ip_address = digitalocean_reserved_ip.visie-reserved-ip.ip_address
   droplet_id = digitalocean_droplet.visie-server.id
